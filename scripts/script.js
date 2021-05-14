61df7675478ee104ccc6673a9ae7b2e70564fe11
Luis Vidal <43793069+LuisVJ@users.noreply.github.com>
2021-05-14 18:44:02 +0200

    Implement basic game loop

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,65 @@ function computerPlay() {
     
 }
 
-alert(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "Scissors") {
+            return "player";
+        }
+        else if (computerSelection === "Paper") {
+            return "computer";
+        }
+        else return "Tie!";
+    }
+    else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "Rock") {
+            return "player";
+        }
+        else if (computerSelection === "Scissors") {
+            return "computer";
+        }
+        else return "Tie!";
+    }
+    else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "Paper") {
+            return "player";
+        }
+        else if (computerSelection === "Rock") {
+            return "computer";
+        }
+        else return "Tie!";
+    }
+    else {
+        return "Invalid option."
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let computerSelection;
+    let result;
+    while (playerScore < 3 && computerScore < 3) {
+        playerSelection = prompt("Choose rock, paper or scissors: ");
+        computerSelection = computerPlay();
+        result = playRound(playerSelection, computerSelection);
+        if (result == "player") {
+            playerScore++;
+            console.log("Player wins round!");
+        }
+        else if (result == "computer") {
+            computerScore++;
+            console.log("Computer wins round");
+        }
+        else {
+            console.log(result);
+        }
+        console.log(`Current score: ${playerScore}:${computerScore}`);
+    }
+
+    let winner = playerScore > computerScore ? "Player" : "Computer";
+    console.log(`Final score is: ${playerScore}:${computerScore}.
+    ${winner} wins!!`)
+}
+
+game();
